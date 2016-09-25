19023694c27127e810ffeebc2919aee180546224
Nicolas PEPIN <nicolas.pepin.06@gmail.com>
2016-09-25 20:14:11 +0200

    Finition de l'initialisation de la map

--- map.cpp
+++ map.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <stdio.h>
+#include <cstdlib>
+#include <ctime>
  
 using namespace std;
 
@@ -8,28 +12,47 @@ const unsigned WIDTH = 512;
 const unsigned HEIGHT = 128;
 const unsigned WIDTHWALL = 16;
 const unsigned WALLOPENINGHEIGHT1 = 8;
-const unsigned WALLOPENINGHEIGHT1 = 16;
+const unsigned WALLOPENINGHEIGHT2 = 16;
 const unsigned WALLDISTANCE = 112;
  
-class map
-{
-    //Initialize all points at 0 (free)
-    vector< vector<int> > map(HEIGHT, vector<int>(WIDTH,0));
+vector< vector<bool> > map(HEIGHT, vector<bool>(WIDTH));
 
+class Map
+{
+    private:
+
+    //typedef vector<vector<bool> >Ground;
+    //Ground map(HEIGHT, std::vector<bool> (WIDTH, 0));
+ 
+    //typedef vector<vector<bool> >Grille;
+    //Grille g(128, std::vector<bool> (512, false));
+    //Initialize all points at 0 (free)
+
+    //vector< vector<bool> > map(HEIGHT, vector<bool>(WIDTH));
+    
+
+    public:
     // Méthodes
     void addWall()
     {
         for(int i = 0; i < HEIGHT; i++){
             for(int j = 0; j < WIDTHWALL; j++){
-                if(i != WALLOPENINGHEIGHT1)
+                if(i < (HEIGHT-WALLOPENINGHEIGHT1)/2 || i >= (HEIGHT+WALLOPENINGHEIGHT1)/2)
+                    map[i][j]=1;
+            }
+            for(int j = 0+WALLDISTANCE; j < WIDTHWALL+WALLDISTANCE; j++){
+                if(i < (HEIGHT-WALLOPENINGHEIGHT2)/2 || i >= (HEIGHT+WALLOPENINGHEIGHT2)/2)
                     map[i][j]=1;
             }
         }
+    }
 
+    void printWall()
+    {
         for(int i = 0; i < HEIGHT; i++){
-            for(int j = 0+WALLDISTANCE; j < WIDTHWALL+WALLDISTANCE; j++){
-                if(i != WALLOPENINGHEIGHT2)
-                    map[i][j]=1;
+            for(int j = 0; j < WIDTH; j++){
+                if(map[i][j]==1)
+                    printf("IL Y A UN MUR EN %d, %d\n", i,j );
             }
         }
     }
@@ -37,17 +60,10 @@ class map
  
 int main()
 {
-    Personnage david, goliath;
-    //Création de 2 objets de type Personnage : david et goliath
- 
-    goliath.attaquer(david);    //goliath attaque david
-    david.boirePotionDeVie(20); //david récupère 20 de vie en buvant une potion
-    goliath.attaquer(david);    //goliath réattaque david
-    david.attaquer(goliath);    //david contre-attaque... c'est assez clair non ?
-    
-    goliath.changerArme("Double hache tranchante vénéneuse de la mort", 40);
-    goliath.attaquer(david);
- 
- 
+    Map map1;
+
+    map1.addWall();
+    map1.printWall();
+
     return 0;
 }
