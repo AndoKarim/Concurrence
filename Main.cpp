5dec0d8c6dea48cfec7bfe484ddb1d9d53f6858e
Ghira anasse <anasse.ghira@etu.unice.fr>
2016-10-04 16:34:40 +0200

    reorganisation of package

--- Main.cpp
+++
@@ -1,7 +0,0 @@
-#include "OptionChecker.h"
-#include "Motor.h"
-
-int main(int argc, char *argv[]) {
-  OptionChecker a = OptionChecker(argc,argv);
-  Motor m = Motor(a.getnbPeople(), a.getnbThread());
-}
