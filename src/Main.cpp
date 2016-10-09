36a75e3c0cfa44dab59e29c19a438e6126ebc7e6
AndoKarim <ando.karim@gmail.com>
2016-10-09 19:21:59 +0200

    start working on running time

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,18 +4,18 @@
 #include <math.h>
 
 
-
 using namespace std;
 
-void initialisation(int nbPlayers, int nbThreads, bool needMeasures){
-  Motor m = Motor(nbPlayers,nbThreads,needMeasures);
+void initialisation(int nbPlayers, int nbThreads, bool needMeasures) {
+  Motor m = Motor(nbPlayers, nbThreads, needMeasures);
 }
 
 
 int main(int argc, char *argv[]) {
-  OptionChecker a = OptionChecker(argc,argv);
-
-  int nbPlayers = (int)pow(2,a.getnbPeople());
+  OptionChecker a = OptionChecker(argc, argv);
+  time_t startTime;
+  time(&startTime);
+  int nbPlayers = (int) pow(2, a.getnbPeople());
   int nbThreads = a.getnbThread();
   bool needMeasures = a.getMeasures();
 
@@ -23,4 +23,11 @@ int main(int argc, char *argv[]) {
   cout << "Nb Thread : " << nbThreads << endl;
 
   initialisation(nbPlayers, nbThreads, needMeasures);
+
+  time_t endTime;
+  time(&endTime);
+
+  double finalTime = difftime(endTime,startTime);
+
+  cout << "Final time : " << finalTime <<endl;
 }
