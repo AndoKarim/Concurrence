755a0e79e39009ec632132a136dd0b2026ae3b90
Ghira anasse <city-ariane@hotmail.fr>
2016-12-04 11:32:48 +0100

    some removes

--- projet2-22/execute.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-###############################################################
-# Team 22
-# Authors: Abdelkarim Andolerzak & Anasse Ghira & Nicolas Pepin
-###############################################################
-path=$(dirname $0)
-
-$path/bin/run -e2 -t1 -p4 -m
